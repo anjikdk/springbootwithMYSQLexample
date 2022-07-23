144f2ab6c8fd38add255798ec86c4a1c34c17a4f
anjikdk <anjikdk@gmail.com>
2022-07-23 11:12:35 +0530

    Initial commit

---
+++ src/main/java/com/sprintboot/mysql/SpringbootwithMysqLexampleApplication.java
@@ -0,0 +1,13 @@
+package com.sprintboot.mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwithMysqLexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootwithMysqLexampleApplication.class, args);
+	}
+
+}
